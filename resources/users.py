d52a7c9945b73041f78f822b752918b07524fda1
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-07-06 15:43:13 -0400

    login user route

--- resources/users.py
+++ resources/users.py
@@ -40,3 +40,35 @@ def user_create():
       message=f"Successfully created a new user",
       status=201
     ), 201
+
+## user/login
+## User login route
+@users.route('/login', methods=["POST"])
+def user_login():
+  payload = request.get_json()
+  payload['username'] = payload['username'].lower() ## usernames are not case sensitive
+  
+  try:
+    user = User.get(User.username == payload['username'])
+    user_dict = model_to_dict(user)
+    if payload['password'] == user_dict['password']: ## Correct password
+      login_user(user)
+      user_dict.pop('password')
+      return jsonify(
+        data=user_dict,
+        message=f"Successfully logged in {user_dict['username']}",
+        status=200
+      ), 200
+    else:  ## Incorrect password
+      return jsonify(
+        data={},
+        message="Incorrect password/username",
+        status=401
+      ), 401
+
+  except models.DoesNotExist:  ## Incorrect username
+    return jsonify(
+      data={},
+      message="Incorrect username/password",
+      status=401
+    ), 401
