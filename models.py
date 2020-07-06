561efc6cbd44b4e7fec2ce5aaaa17ec5fc71edc4
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-07-06 15:28:58 -0400

    set up job seeker profile model

--- models.py
+++ models.py
@@ -7,12 +7,28 @@ class User(UserMixin, Model):
   username=CharField(unique=True)
   password=CharField()
   jobseeker=BooleanField() ## True if the User is a Job seeker
+  
+  class Meta:
+    database = DATABASE
+
+class JobSeekerInfo(Model):
+  education=CharField()
+  name=CharField()
+  date_of_birth=DateField()
+  email=CharField()
+  location=CharField()
+  language=CharField()
+  ethinicity=CharField()
+  skillset=CharField()
+  industry=CharField()
+  payrange=CharField()
+
   class Meta:
     database = DATABASE
 
 def initialize():
   DATABASE.connect()
-  DATABASE.create_tables([User], safe=True)
+  DATABASE.create_tables([User, JobSeekerInfo], safe=True)
   print('connected to database')
 
   DATABASE.close()
